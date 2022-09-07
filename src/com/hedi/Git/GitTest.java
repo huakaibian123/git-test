f3efdb82743f50c20b651c32a89e136362163fb6
hedi <1930763303@qq.com>
2022-09-07 15:49:15 +0800

    third commit

--- src/com/hedi/Git/GitTest.java
+++ src/com/hedi/Git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("你好，中国");
         System.out.println("你好，何迪");
+        System.out.println("你好，华为");
     }
 }
