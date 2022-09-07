f7ea35745fff9a2a4d0eae207dd97b4844397b4b
hedi <1930763303@qq.com>
2022-09-07 17:34:37 +0800

    gitee commit1

--- src/com/hedi/Git/GitTest.java
+++ src/com/hedi/Git/GitTest.java
@@ -8,7 +8,6 @@ public class GitTest {
         System.out.println("你好，java");
         System.out.println("master test");
         System.out.println("hot-fix commit2");
-        System.out.println("push commit");
-        System.out.println("pull commit");
+        System.out.println("push commit gitee");
     }
 }
